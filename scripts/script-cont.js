1e73d5612cc542314fa8146514f9ca4c0ad01159
Lucas Molina <lucasivanmolina_bass@hotmail.com>
2023-05-13 16:52:41 -0300

    inicializando-repo-tpo-cac

---
+++ scripts/script-cont.js
@@ -0,0 +1,20 @@
+const encabezado = `
+<div class="cont-header">
+    <div class="logo">
+        <!--<img src="img/logo.svg" alt="logo"-->
+        <h1><a href="index.html">Devrel</a></h1>
+    </div>
+    <div class="nav-bar">
+        <nav>
+            <ul>
+                <li><a href="">NOSOTROS</a></li>
+                <li><a href="">ALOJAMIENTO</a></li>
+                <li><a href="">CONTACTO</a></li>
+            </ul>
+        </nav>
+        <button>RESERVACIONES</button>
+    </div>
+</div>
+`;
+
+document.querySelector("#header").innerHTML = encabezado;
---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+var fecha = new Date().toISOString().substr(0, 10);
+document.getElementById("check-in").value = fecha;
+document.getElementById("check-out").value = fecha;
+
+function sumarPersona() {
+  var input = document.getElementById("personas");
+  input.value = parseInt(input.value)+1;
+}
+
+function restarPersona() {
+  var input = document.getElementById("personas");
+  if (input.value > 1) {
+    input.value = parseInt(input.value)-1;
+  }
+}
+
+document.getElementById("sumar").addEventListener("click", sumarPersona);
+document.getElementById("restar").addEventListener("click", restarPersona);
