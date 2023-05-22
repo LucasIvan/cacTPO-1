6b3d0831467c0dc5a9a9fdb6b165d9a351f129e8
Lucas Molina <lucasivanmolina_bass@hotmail.com>
2023-05-22 00:14:42 -0300

    agregado-alojamiento-nosotros

--- scripts/script-cont.js
+++ scripts/script-cont.js
@@ -1,14 +1,14 @@
 const encabezado = `
 <div class="cont-header">
     <div class="logo">
-        <h1><a href="index.html">Devrel</a></h1>
+        <h1 class="titulo"><a href="index.html">Devrel</a></h1>
     </div>
     <div class="nav-bar">
         <nav>
             <ul>
-                <li><a href="">NOSOTROS</a></li>
-                <li><a href="">ALOJAMIENTO</a></li>
-                <li><a href="">CONTACTO</a></li>
+                <li><a href="./alojamiento.html">ALOJAMIENTO</a></li>
+                <li><a href="./nosotros.html">NOSOTROS</a></li>
+                <li><a href="./contacto.html">CONTACTO</a></li>
             </ul>
         </nav>
         <button>RESERVACIONES</button>
@@ -16,4 +16,4 @@ const encabezado = `
 </div>
 `;
 
-document.querySelector("#header").innerHTML = encabezado;
+document.querySelector(".header").innerHTML = encabezado;
